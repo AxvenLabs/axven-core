a7c42199b5d1dacfa8b5e2ae5c530afced4cbdbe
AxvenLabs <dogacan432@gmail.com>
2026-08-31 14:02:13 +0300

    SEC-214: gate node2 console on validated runtime

--- axven-console-node2.cmd
+++ axven-console-node2.cmd
@@ -1,3 +1,5 @@
 @echo off
 cd /d "%~dp0"
+powershell.exe -NoLogo -NoProfile -NonInteractive -File "%~dp0ensure_runtime.ps1"
+if errorlevel 1 exit /b 1
 .venv\Scripts\python.exe axven_console.py --datadir canonical-node2 --rpc-port 18453
